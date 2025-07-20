a5af3e2feae5aab7c2287c90430c69abc79a36a9
shashidhar078 <nagunurishashidhar@gmail.com>
2025-07-20 23:31:30 +0530

    exploring slicing

--- 01Basics/chai.py
+++ 01Basics/chai.py
@@ -1,3 +1,37 @@
 from chai_aur import chai
 
 chai("ginger tea")
+
+#converting list to string 
+
+chai_variety=["Lemon","Ginger","Masala"];
+print("".join(chai_variety))
+print(" ".join(chai_variety))
+print("-".join(chai_variety))
+print(",".join(chai_variety))
+
+#you can convert and represent it however you need
+
+#slicing examples on strings
+chai = "Ginger Tea"
+print(chai[:])
+print(chai[:len(chai)])
+print(chai[1:5])
+print(chai[:5])
+print(chai[2:])
+print(chai[::2])
+
+#Slicing examples in list 
+chai_types=["lemon","ginger","masala","herbal"]
+print(chai_types)
+print(chai_types[:])
+print(chai_types[:len(chai_types)])
+print(chai_types[1:5])
+print(chai_types[:5])
+print(chai_types[2:])
+print(chai_types[::2])
+
+
+#Assigning content to slice 
+chai_types[1:2]=["Oolong"]
+print(chai_types)
